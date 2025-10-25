147f83337245dccfd3d34899b24c5a3f7df44a0d
Lion¨ <work@lionsix.ch>
2025-10-25 18:30:04 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Die zahl ist {count}
+          Die veränderung ist {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
