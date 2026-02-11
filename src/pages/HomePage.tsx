ff4ac56c255fe47d466c0dfdfbfde288e7a1eec2
snow2405 <hey@lionsix.ch>
2026-02-11 23:17:19 +0100

    refactor: remove AGBSection import and clean up Vite config by removing unused server settings

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import HeroSection from '../sections/HeroSection';
 import FeaturesSection from '../sections/FeaturesSection';
 import SubBanner from '../sections/SubBanner';
-import AGBSection from '../sections/AGBSection';
 import Footer from '../sections/Footer';
 import PhoneSignupForm from '../components/PhoneSignupForm';
 
